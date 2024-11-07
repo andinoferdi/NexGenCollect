8a675772e3c05448ea8b8a41492e15ae1a743bf4
andinoferdi <andinoferdiansah@gmail.com>
2024-11-07 14:51:48 +0700

    Penambahan tampilan user dan admin menggunakan template metronic

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view(view: 'userpage.index');
 });
 
 Route::get('/dashboard', function () {
-    return view('welcome');
+    return view(view: 'dashboard.index');
 });
